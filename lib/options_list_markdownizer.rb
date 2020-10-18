c8e350e5b3f75e9aa235919f71fe204f022f58ad
Jakub Pavlik <severus@post.cz>
2020-10-18 12:56:18 +0200

    extract implementation of `options_list`

---
+++ lib/options_list_markdownizer.rb
@@ -0,0 +1,35 @@
+class OptionsListMarkdownizer
+  def call(command)
+    ui = Gem::SilentUI.new
+    Gem::DefaultUserInteraction.use_ui ui do
+      # Invoke the Ruby options parser by asking for help. Otherwise the
+      # options list in the parser will never be initialized.
+      command.show_help
+    end
+
+    parser = command.send(:parser)
+    options = ''
+    helplines = parser.summarize
+    helplines.each do |helpline|
+      break if (helpline =~ /Arguments/) || (helpline =~  /Summary/)
+      unless helpline.gsub(/\n/, '').strip == ''
+        # Use zero-width space to prevent "helpful" change of -- to &ndash;
+        helpline = helpline.gsub('--', '-&#8203;-').gsub('[', '\\[').gsub(']', '\\]')
+
+        if helpline =~ /^\s{10,}(.*)/
+          options = options[0..-2] + " #{$1}\n"
+        else
+          if helpline =~ /^(.+)\s{2,}(.*)/
+            helpline = "#{$1} - #{$2}"
+          end
+          if helpline =~ /options/i
+            options += "\n### #{helpline.strip.delete_suffix(":")}\n\n"
+          else
+            options += "* #{helpline.strip}\n"
+          end
+        end
+      end
+    end
+    options
+  end
+end
